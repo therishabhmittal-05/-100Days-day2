8f69d84f0bc2e224f5672ef11f89653a13106ac1
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-01-23 22:12:40 +0530

    nikelp

---
+++ nikelp/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-[#414141] grid place-items-center h-screen">
+          <div className="text-4xl font-bold text-white font-mono">Hello World</div>
+    </main>
+  );
+}
